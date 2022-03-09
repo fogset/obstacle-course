70bed7e666cdd87824c79cb7426c19315de5eebf
Tianhao Zhang <zhangth1990@hotmail.com>
2022-03-09 01:05:34 -0800

    added Collision detection

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -8,12 +8,16 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer(){
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue , 0, zValue );
